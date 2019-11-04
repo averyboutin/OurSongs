ceb311d92a3e11138bf591b3357274d2b64a7503
Avery Boutin <averyboutin@Averys-MacBook-Pro-3.local>
2019-11-04 17:13:57 -0500

    Modified app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const http = require('http');
-
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Welcome to OurSongs!\n');
-});
+const server = http.createServer(response);
+
+function response(req, res) {
+    let file = "";
+    if (req.url == "/") {
+        file = __dirname + "/index.html";
+    } else {
+        file = __dirname + req.url;
+    }
+
+    fs.readFile(file, function (err, page) {
+        if (err) {
+            res.writeHead(404);
+            return res.end("Page not found!");
+        }
+        res.writeHead(200);
+        res.end(page);
+    });
+}
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
